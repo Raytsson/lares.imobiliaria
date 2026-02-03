44db5a3c2ebf8b36b9544ef93825e1c1483eb816
Gabriel Matheus <gabrielmath14@outlook.com>
2026-02-03 00:31:07 -0300

    .

--- src/main/java/br/imob/GerarHash.java
+++ src/main/java/br/imob/GerarHash.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GerarHash {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String hash = encoder.encode("123456");
+        String hash = encoder.encode("senhadaviviana123");
         System.out.println(hash);
     }
 }
--- src/main/java/br/imob/configuration/SecurityConfig.java
+++ src/main/java/br/imob/configuration/SecurityConfig.java
@@ -132,7 +132,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200",
+                "http://localhost:3000",
+                "https://site-barrios.vps7358.panel.icontainer.net"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
